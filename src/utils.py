8c4801bbfc8b09b0a229fe9975d6105b5a55d063
Sele Okojie <SeleOkojie4@gmail.com>
2025-04-24 04:58:47 -0400

    Adjust text rendering parameters in utils.py for improved label visibility

--- src/utils.py
+++ src/utils.py
@@ -6,5 +6,5 @@ def annotate_frame(frame, label, position=(30,30)):
     Draw label on frame
     """
     cv2.putText(frame, label, position, cv2.FONT_HERSHEY_SIMPLEX,
-                1.0, (0,255,0), 2, cv2.LINE_AA)
+                0.6, (0,255,0), 1, cv2.LINE_AA)
     return frame
